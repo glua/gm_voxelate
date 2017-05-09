4a9648132a74337faa283240cde0e7a70b62948b
Swad <SwadicalRag@users.noreply.github.com>
2017-05-09 14:37:19 +0930

    remove debug prints and print init message

--- lua/init.lua
+++ lua/init.lua
@@ -1,11 +1,9 @@
 local runtime = dofile("runtime.lua")
 
-print("HELLO WORLD!")
-
-print("sn_bf_write",sn_bf_write)
-
 gm_voxelate = runtime.require("./voxelate").Voxelate
 
 gm_voxelate.io:SetDebug(true)
 
 runtime.require("./voxelate/entities/voxelworld")
+
+gm_voxelate.io:Print("Lua initialisation complete")
