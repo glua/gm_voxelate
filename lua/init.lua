950470bf579ef3e4889b83ef26c666464583a980
Swad <SwadicalRag@users.noreply.github.com>
2017-05-09 22:17:41 +0930

    have a separate lua version number

--- lua/init.lua
+++ lua/init.lua
@@ -2,8 +2,10 @@ local runtime = dofile("runtime.lua")
 
 gm_voxelate = runtime.require("./voxelate").Voxelate
 
+gm_voxelate.LuaVersion = "V0.2.0"
+
 gm_voxelate.io:SetDebug(true)
 
 runtime.require("./voxelate/entities/voxelworld")
 
-gm_voxelate.io:Print("Lua initialisation complete")
+gm_voxelate.io:Print("Lua initialisation complete (%s)",gm_voxelate.LuaVersion)
