313494c6554ef610c9dff427c9fc64f043a81196
Swad <SwadicalRag@users.noreply.github.com>
2017-05-09 17:25:37 +0930

    add bON as a dependency for networking

---
+++ lua/voxelate/serialization.lua
@@ -0,0 +1,19 @@
+local runtime,exports = ...
+
+local bON = dofile("deps/bon.lua")
+
+function exports.serialize(data)
+    return bON.serialize(data)
+end
+
+function exports.deserialize(data)
+    return bON.deserialize(data)
+end
+
+-- function exports.serialize(data)
+--     return util.TableToJSON(data)
+-- end
+
+-- function exports.deserialize(data)
+--     return util.JSONToTable(data)
+-- end
