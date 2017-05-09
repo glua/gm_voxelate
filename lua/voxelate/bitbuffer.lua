365b4af024d1cda7221677525f5d65b643ae3448
Swad <SwadicalRag@users.noreply.github.com>
2017-05-09 21:34:14 +0930

    Try and fix the bitbuf byte allignment assertions

--- lua/voxelate/bitbuffer.lua
+++ lua/voxelate/bitbuffer.lua
@@ -6,12 +6,18 @@ local sn_bf_read = sn_bf_read
 local sn_bf_write = sn_bf_write
 
 function exports.Writer(bytes)
-    local ucptr = UCHARPTR(bytes)
+    local gap = 4 - bytes % 4
+
+    local ucptr = UCHARPTR(bytes + gap)
 
     return sn_bf_write(ucptr)
 end
 
 function exports.Reader(data)
+    local gap = 4 - #data % 4
+
+    data = data..("\x00"):rep(gap)
+
     local ucptr = UCHARPTR_FromString(data,#data)
 
     return sn_bf_read(ucptr)
