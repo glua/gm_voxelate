95f05336924ffbc264420282dcf85c216f227055
Adam Coggeshall <dameerkat@gmail.com>
2017-05-12 19:35:53 -0600

    Don't let players move chunks.

--- lua/voxelate/init.lua
+++ lua/voxelate/init.lua
@@ -55,3 +55,12 @@ function Voxelate:SetWorldConfig(index,config)
 end
 
 exports.Voxelate = Voxelate:__new()
+
+-- Don't let players screw with our voxels!
+hook.Add("PhysgunPickup", "Voxelate.NoPhysgun", function(ply,ent)
+	if ent:GetClass() == "voxel_world" then return false end
+end)
+
+hook.Add("CanTool", "Voxelate.NoTool", function(ply,tr,tool)
+	if IsValid( tr.Entity ) and tr.Entity:GetClass() == "voxel_world" then return false end
+end)
