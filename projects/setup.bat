fdb6d2bef11020f4c9acf86bf16eae9dbd1fa934
Adam Coggeshall <dameerkat@gmail.com>
2017-05-13 16:58:21 -0600

    Add security risk to my premake batch script.

--- projects/setup.bat
+++ projects/setup.bat
@@ -1,2 +1,2 @@
 rem Script to run premake assuming everything is cloned into the same directory.
-premake5 vs2017 --gmcommon=..\..\garrysmod_common --sourcesdk=..\..\sourcesdk-minimal --autoinstall
+premake5 vs2017 --gmcommon=..\..\garrysmod_common --sourcesdk=..\..\sourcesdk-minimal --autoinstall --luahotloading
