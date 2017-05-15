ac4de71285ac38cae5dd875b5dec2e7e55721e7f
Swad <SwadicalRag@users.noreply.github.com>
2017-05-15 17:14:06 +0930

    Add functions to expose the voxelate runtime

--- lua/init.lua
+++ lua/init.lua
@@ -7,3 +7,11 @@ gm_voxelate.LuaVersion = "V0.3.0"
 gm_voxelate.io:SetDebug(true)
 
 gm_voxelate.io:Print("Lua initialisation complete (%s)",gm_voxelate.LuaVersion)
+
+function gm_voxelate:GetVoxelateRuntime()
+	return runtime
+end
+
+function gm_voxelate:GetNewRuntime()
+	return dofile("runtime.lua")
+end
