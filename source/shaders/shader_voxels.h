e0d34c1e9ae3cd595b680f559a2a5b0d7ef98733
Adam Coggeshall <dameerkat@gmail.com>
2017-05-26 00:39:13 -0600

    We got textures!

--- source/shaders/shader_voxels.h
+++ source/shaders/shader_voxels.h
@@ -10,7 +10,7 @@ END_SHADER_PARAMS
 
 SHADER_INIT
 {
-
+	LoadTexture(BASETEXTURE, TEXTUREFLAGS_SRGB);
 }
 
 SHADER_FALLBACK
@@ -22,7 +22,13 @@ SHADER_DRAW
 {
 	SHADOW_STATE
 	{
+
 		pShaderShadow->VertexShaderVertexFormat(VERTEX_POSITION | VERTEX_NORMAL, 1, 0, 0);
+		
+		pShaderShadow->EnableTexture(SHADER_SAMPLER0, true);
+		pShaderShadow->EnableSRGBRead(SHADER_SAMPLER0, true);
+
+		pShaderShadow->EnableSRGBWrite(true);
 
 		DECLARE_STATIC_VERTEX_SHADER(voxels_vs20);
 		SET_STATIC_VERTEX_SHADER(voxels_vs20);
@@ -33,8 +39,9 @@ SHADER_DRAW
 	
 	DYNAMIC_STATE
 	{
+		BindTexture(SHADER_SAMPLER0, BASETEXTURE);
+
 		pShaderAPI->SetVertexShaderStateAmbientLightCube();
-		//WpShaderAPI->SetPixelShaderStateAmbientLightCube(4);
 		
 		DECLARE_DYNAMIC_VERTEX_SHADER(voxels_vs20);
 		SET_DYNAMIC_VERTEX_SHADER(voxels_vs20);
