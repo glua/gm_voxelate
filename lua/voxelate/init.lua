d276065272ffec3b2896605bf5d908a76ede64d7
Adam Coggeshall <dameerkat@gmail.com>
2017-06-18 19:16:30 -0600

    Add back anti physgun / tool.

--- lua/voxelate/init.lua
+++ lua/voxelate/init.lua
@@ -3,3 +3,12 @@ require("bulkhelpers")
 
 require("networking")
 require("entities/voxelworld")
+
+-- Don't let players screw with our voxels! 
+hook.Add("PhysgunPickup", "Voxelate.NoPhysgun", function(ply,ent) 
+  if ent:GetClass() == "voxel_world" then return false end 
+end) 
+ 
+hook.Add("CanTool", "Voxelate.NoTool", function(ply,tr,tool) 
+  if IsValid( tr.Entity ) and tr.Entity:GetClass() == "voxel_world" then return false end 
+end) 
