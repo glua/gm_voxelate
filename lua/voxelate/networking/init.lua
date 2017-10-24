160080bf6806ce7980507a26addbf327154f027c
swadical <swadical@lab.glua.team>
2017-10-25 00:35:34 +1030

    more formatting

--- lua/voxelate/networking/init.lua
+++ lua/voxelate/networking/init.lua
@@ -1,14 +1,10 @@
-
-
 internals.netListeners = {}
-
 
 hook.Add("Tick","Voxelate.Networking.Polling",function()
 	internals.networkPoll()
 end)
 
 hook.Add("VoxNetworkPacket","Voxelate.Networking",function(peerID,channelID,payloadData)
-
 	if internals.netListeners[channelID] then
 		internals.netListeners[channelID](payloadData, peerID)
 	else
